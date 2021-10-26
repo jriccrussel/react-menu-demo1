d64fbe24f247642bff3f8f4c0b5223143fe03ac2
jriccrussel <jriccrussel@gmail.com>
2021-10-26 22:48:11 +0800

    complete toggle navigation & nav animation

--- src/component/Hamburger.js
+++ src/component/Hamburger.js
@@ -1,31 +1,109 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import gsap from 'gsap'
 import { Link } from 'react-router-dom'
 
-const Hamburger = () => {
-    return (
-        <div className="hamburger-menu">
-            <div className="menu-secondary-background-color"></div>
-            <div className="menu-layer">
-                <div className="menu-city-background">
+const Hamburger = ({state}) => {
+    // Ref for animated dom nodes
+    let menu = useRef(null)
+    let showMenu = useRef(null)
+    let showBg = useRef(null)
+    let bg = useRef(null)
+    let line1 = useRef(null)
+    let line2 = useRef(null)
+    let line3 = useRef(null)
+    let info = useRef(null)
 
-                </div>
+    useEffect(() => {
+        if (state.clicked === false){
+            // Close Menu
+            gsap.to([showMenu.current, showBg.current], {
+                duration: 0.8,
+                height: 0,
+                ease: "power3.inOut",
+                stagger: {
+                    amount: 0.07
+                }
+            })
+            gsap.to(menu.current, { duration: 1, css: { display: "none" } })
+
+            // Close Menu without Animation 
+            // menu.current.style.display = "none"
+
+        } else if (state.clicked === true || (state.clicked === true && state.initial === null)){
+            // Open Menu
+            gsap.to(menu.current, { duration: 0, css: { display: "block" } })
+            gsap.to([showMenu.current, showBg.current], {
+                duration: 0,
+                opacity: 1,
+                height: "100%",
+            })
+            staggerShow(showMenu.current, showBg.current)
+            fadeInUp(info.current)
+            staggerTxt(line1.current, line2.current, line3.current)
+            // Open Menu without Animation 
+            // menu.current.style.display = "block"
+        }
+    }, [state])
+
+    // ANIMATIONS
+    const staggerShow = (node1, node2) => {
+        gsap.from([node1, node2], {
+            duration: 0.8,
+            height: 0,
+            transformOrigin: "right top",
+            skewY: 2,
+            ease: "power3.inOut",
+            stagger: {
+                amount: 0.1
+            }
+        })
+    }
+
+    const staggerTxt = (node1, node2, node3) => {
+        gsap.from([node1, node2, node3], {
+            duration: 0.8,
+            y: 100,
+            delay: 0.1,
+            ease: "power3.inOut",
+            stagger: {
+                amount: 0.3           
+            }
+        })
+    }
+    
+    const fadeInUp = (node) => {
+        gsap.from(node, {
+            y: 60,
+            duration: 1,
+            delay: 0.2,
+            opacity: 0,
+            ease: "power3.inOut"
+        })
+    }
+
+    return (
+        // ref={el => (menu = el)}
+        <div ref={menu} className="hamburger-menu">
+            <div ref={showBg} className="menu-secondary-background-color"></div>
+            <div ref={showMenu} className="menu-layer">
+                <div className="menu-city-background"></div>
                 <div className="container">
                     <div className="wrapper">
-                        <div className="menu-links">
+                        <div className="menu-links flex align-center space-between">
                             <nav>
                                 <ul>
                                     <li>
-                                        <Link to="/oppurtunies">Opportunity</Link>
+                                        <Link ref={line1} to="/oppurtunies">Opportunity</Link>
                                     </li>
                                     <li>
-                                        <Link to="/solutions">Opportunity</Link>
+                                        <Link ref={line2} to="/solutions">Opportunity</Link>
                                     </li>
                                     <li>
-                                        <Link to="/contact-us">Contact us</Link>
+                                        <Link ref={line3} to="/contact-us">Contact us</Link>
                                     </li>
                                 </ul>
                             </nav>
-                            <div className="info">
+                            <div ref={info} className="info">
                                 <h3>Our Promis</h3>
                                 <p>
                                 The passage experienced a surge in popularity during the 1960s
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { withRouter, Link } from 'react-router-dom'
+import Hamburger from './Hamburger'
 
-const Header = () => {
+const Header = ({history}) => {
+    // Menu State 
+    const [state, setState] = useState({
+        initial: false,
+        clicked: null,
+        menuName: "Menu"
+    })
+
+    // Button State
+    const [disabled, setDisabled] = useState(false)
+
+    //Use Effect
+    useEffect(() => {
+        //Listening for page changes.
+        history.listen(() => {
+        setState({ clicked: false, menuName: "Menu" })
+        })
+    }, [history])
+
+    // Toggle Menu
+    const handleMenu = () => {
+        disableMenu()
+        if (state.initial === false) {
+            setState({
+                initial: null,
+                clicked: true,
+                menuName: "Close"
+            })
+        } else if (state.clicked === true) {
+            setState({
+                clicked: !state.clicked,
+                menuName: "Menu"
+            })
+        } else if (state.clicked === false) {
+            setState({
+                clicked: !state.clicked,
+                menuName: "Close"
+            })
+        }
+    }
+
+    // Determine if the menu button should be disable
+    const disableMenu = () => {
+        setDisabled(!disabled);
+        setTimeout(() => {
+          setDisabled(false)
+        }, 1200)
+    }
+
+
     return (
         <header>
             <div className="container">
@@ -11,13 +61,16 @@ const Header = () => {
                             <Link to="/">HAMBRG.</Link>
                         </div>
                         <div className="menu">
-                            <button>MENU</button>
+                            <button disabled={disabled} onClick={handleMenu}>
+                                {state.menuName}
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
+            <Hamburger state={state} />
         </header>
     )
 }
 
-export default Header
+export default withRouter(Header)

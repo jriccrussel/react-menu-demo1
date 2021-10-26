7701d9a23a09fc6560745680b45fd21c069211aa
jriccrussel <jriccrussel@gmail.com>
2021-10-26 23:27:42 +0800

    complete skew link

--- src/component/Animation.js
+++ src/component/Animation.js
@@ -1,12 +1,82 @@
-import React from 'react'
 import gsap from 'gsap'
 
-export default function Animation() {
-    return (
-        <div>
-            
-        </div>
-    )
+export const staggerShow = (node1, node2) => {
+    gsap.from([node1, node2], {
+        duration: 0.8,
+        height: 0,
+        transformOrigin: "right top",
+        skewY: 2,
+        ease: "power3.inOut",
+        stagger: {
+            amount: 0.1
+        }
+    })
+}
+
+export const staggerTxt = (node1, node2, node3) => {
+    gsap.from([node1, node2, node3], {
+        duration: 0.8,
+        y: 100,
+        delay: 0.1,
+        ease: "power3.inOut",
+        stagger: {
+            amount: 0.3           
+        }
+    })
+}
+
+export const fadeInUp = (node) => {
+    gsap.from(node, {
+        y: 60,
+        duration: 1,
+        delay: 0.2,
+        opacity: 0,
+        ease: "power3.inOut"
+    })
+}
+
+// export const handleCity = (node) => {
+//     gsap.to(bg.current, {
+//         duration: 0,
+//         // background: `url(${node}) center center`,
+//         background: `url(${node})`,
+//     })
+//     gsap.to(bg.current, {
+//         duration: 0.4,
+//         opacity: 1,
+//         ease: "power3.inOut"
+//     })
+//     gsap.to(bg.current, {
+//         duration: 0.4,
+//         // scaleY: 2,\
+//         scaleY: 1,
+//         transformOrigin: "right top"
+//     })
+// }
+
+// export const handleCityReturn = () => {
+//     gsap.to(bg.current, {
+//         duration: 0.4,
+//         opacity: 0,
+//     })
+// }
+
+export const handleHover = (e) => {
+    gsap.to(e.target, {
+        duration: 0.3,
+        y: 3,
+        skewX: 4,
+        ease: "power1.inOut"
+    })
+}
+
+export const handleHoverExit = (e) => {
+    gsap.to(e.target, {
+        duration: 0.3,
+        y: -3,
+        skewX: 0,
+        ease: "power1.inOut"
+    })
 }
 
 
--- src/component/Hamburger.js
+++ src/component/Hamburger.js
@@ -7,6 +7,15 @@ import austin from '../assets/img/austin.webp'
 import newyork from '../assets/img/newyork.webp'
 import sanfrancisco from '../assets/img/sanfrancisco.webp'
 import hongKong from '../assets/img/hong-kong.webp'
+import {
+    staggerShow,
+    staggerTxt,
+    fadeInUp,
+    handleCity,
+    handleCityReturn,
+    handleHover,
+    handleHoverExit
+} from './Animation'
 
 const cities = [
     {name: "Dallas", image: dallas},
@@ -57,43 +66,44 @@ const Hamburger = ({state}) => {
             // Open Menu without Animation 
             // menu.current.style.display = "block"
         }
+        
     }, [state])
 
     // ANIMATIONS
-    const staggerShow = (node1, node2) => {
-        gsap.from([node1, node2], {
-            duration: 0.8,
-            height: 0,
-            transformOrigin: "right top",
-            skewY: 2,
-            ease: "power3.inOut",
-            stagger: {
-                amount: 0.1
-            }
-        })
-    }
+    // const staggerShow = (node1, node2) => {
+    //     gsap.from([node1, node2], {
+    //         duration: 0.8,
+    //         height: 0,
+    //         transformOrigin: "right top",
+    //         skewY: 2,
+    //         ease: "power3.inOut",
+    //         stagger: {
+    //             amount: 0.1
+    //         }
+    //     })
+    // }
 
-    const staggerTxt = (node1, node2, node3) => {
-        gsap.from([node1, node2, node3], {
-            duration: 0.8,
-            y: 100,
-            delay: 0.1,
-            ease: "power3.inOut",
-            stagger: {
-                amount: 0.3           
-            }
-        })
-    }
+    // const staggerTxt = (node1, node2, node3) => {
+    //     gsap.from([node1, node2, node3], {
+    //         duration: 0.8,
+    //         y: 100,
+    //         delay: 0.1,
+    //         ease: "power3.inOut",
+    //         stagger: {
+    //             amount: 0.3           
+    //         }
+    //     })
+    // }
     
-    const fadeInUp = (node) => {
-        gsap.from(node, {
-            y: 60,
-            duration: 1,
-            delay: 0.2,
-            opacity: 0,
-            ease: "power3.inOut"
-        })
-    }
+    // const fadeInUp = (node) => {
+    //     gsap.from(node, {
+    //         y: 60,
+    //         duration: 1,
+    //         delay: 0.2,
+    //         opacity: 0,
+    //         ease: "power3.inOut"
+    //     })
+    // }
 
     const handleCity = (node) => {
         gsap.to(bg.current, {
@@ -121,6 +131,24 @@ const Hamburger = ({state}) => {
         })
     }
 
+    // const handleHover = (e) => {
+    //     gsap.to(e.target, {
+    //         duration: 0.3,
+    //         y: 3,
+    //         skewX: 4,
+    //         ease: "power1.inOut"
+    //     })
+    // }
+
+    // const handleHoverExit = (e) => {
+    //     gsap.to(e.target, {
+    //         duration: 0.3,
+    //         y: -3,
+    //         skewX: 0,
+    //         ease: "power1.inOut"
+    //     })
+    // }
+
     return (
         // ref={el => (menu = el)}
         <div ref={menu} className="hamburger-menu">
@@ -133,13 +161,34 @@ const Hamburger = ({state}) => {
                             <nav>
                                 <ul>
                                     <li>
-                                        <Link ref={line1} to="/oppurtunies">Opportunity</Link>
+                                        <Link
+                                            onMouseEnter={e => handleHover(e)}
+                                            onMouseOut={e => handleHoverExit(e)}
+                                            ref={line1}
+                                            to="/oppurtunies"
+                                        >
+                                            Opportunity
+                                        </Link>
                                     </li>
                                     <li>
-                                        <Link ref={line2} to="/solutions">Solutions</Link>
+                                        <Link
+                                            onMouseEnter={e => handleHover(e)}
+                                            onMouseOut={e => handleHoverExit(e)}
+                                            ref={line2}
+                                            to="/solutions"
+                                        >
+                                            Solutions
+                                        </Link>
                                     </li>
                                     <li>
-                                        <Link ref={line3} to="/contact-us">Contact us</Link>
+                                        <Link
+                                            onMouseEnter={e => handleHover(e)}
+                                            onMouseOut={e => handleHoverExit(e)}
+                                            ref={line3}
+                                            to="/contact-us"
+                                        >
+                                            Contact us
+                                        </Link>
                                     </li>
                                 </ul>
                             </nav>
